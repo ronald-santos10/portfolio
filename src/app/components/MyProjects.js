1c8273dd7b029978c03b6c3f9c6522c0a5a5c4f3
Ronald Santos <ronalddesign10@gmail.com>
2024-03-27 13:07:26 -0300

    estilização até os projetos

--- src/app/components/MyProjects.js
+++ src/app/components/MyProjects.js
@@ -4,17 +4,20 @@ import './styles/MyProjects.scss'
 function MyProjects(){
     return(
         <div className='projects'>
-            <ul className='project-list'>
+            <ul className='projectlist'>
                 {data.map(item => (
                 <li key={item.id}>
                     <img src={item.image}/>
-                    <h2>{item.title}</h2>
-                    <p>{item.description}</p>
-                    <p>{item.stacks}</p>
-                    <div className='buttons'>
+                    <div className='info'>
+                        <h2>{item.title}</h2>
+                        <p>{item.description}</p>
+                        <h4>{item.stacks}</h4>
+                        <div className='buttons'>
                         <button className='btn-deploy'><a href={item.deploy} target="_blank">Visualizar</a></button>
-                        <button className='btn-github'><a href={item.github} target="_blank">Visualizar</a></button>
+                        <button className='btn-github'><a href={item.github} target="_blank">Github</a></button>
                     </div>
+                    </div>
+                    
                 </li>
                 ))}
             </ul>
--- src/app/components/Stacks.js
+++ src/app/components/Stacks.js
@@ -9,23 +9,24 @@ import node from '../../../public/logo Node.svg';
 import next from '../../../public/logo Nextjs.svg';
 import sass from '../../../public/logo Sass.svg';
 import git from '../../../public/logo Git.svg';
+import './styles/Stacks.scss';
 
 
 function Stacks(){
     return(
         <div className='stacks'>
-            <h2>Minhas Stacks e Tecnologias</h2>
-            <div className='stackList'>
-                <Image src={javascript} alt='logo javascript'/>
-                <Image src={typescript} alt='logo javascript'/>
-                <Image src={react} alt='logo javascript'/>
-                <Image src={html} alt='logo javascript'/>
-                <Image src={css} alt='logo javascript'/>
-                <Image src={mysql} alt='logo javascript'/>
-                <Image src={node} alt='logo javascript'/>
-                <Image src={next} alt='logo javascript'/>
-                <Image src={sass} alt='logo javascript'/>
-                <Image src={git} alt='logo javascript'/>
+            <h2>Minhas <strong>Stacks</strong> e <strong>Tecnologias</strong></h2>
+            <div className='list'>
+                <Image src={javascript} alt='logo javascript' className='item'/>
+                <Image src={typescript} alt='logo javascript' className='item'/>
+                <Image src={react} alt='logo javascript' className='item'/>
+                <Image src={html} alt='logo javascript' className='item'/>
+                <Image src={css} alt='logo javascript' className='item'/>
+                <Image src={mysql} alt='logo javascript' className='item'/>
+                <Image src={node} alt='logo javascript' className='item'/>
+                <Image src={next} alt='logo javascript' className='item'/>
+                <Image src={sass} alt='logo javascript' className='item'/>
+                <Image src={git} alt='logo javascript' className='item'/>
             </div>
         </div>
     )
