b436936a94231d431cfe752abfcd64f1e53f02b4
Ronald Santos <ronalddesign10@gmail.com>
2024-04-03 10:41:59 -0300

    ajustes media query

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@ import './styles/Footer.scss'
 
 function Footer(){
     return (
-        <div>
+        <div className='footer'>
         <h3>Feito por <strong className='bold'>Ronald Santos</strong></h3>
       </div>
     )
