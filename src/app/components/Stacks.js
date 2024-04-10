121bec1d01d786eaa1b16b4c4d4c834c1d4ed186
Ronald Santos <ronalddesign10@gmail.com>
2024-04-10 10:38:11 -0300

    correção de alt

--- src/app/components/Stacks.js
+++ src/app/components/Stacks.js
@@ -4,7 +4,7 @@ import typescript from '../../../public/logo Typescript.svg';
 import react from '../../../public/logo React.svg';
 import html from '../../../public/logo HTML.svg';
 import css from '../../../public/logo CSS.svg';
-import mysql from '../../../public/logo MySql.svg';
+import tailwind from '../../../public/logo tailwind.svg';
 import node from '../../../public/logo Node.svg';
 import next from '../../../public/logo Nextjs.svg';
 import sass from '../../../public/logo Sass.svg';
@@ -18,15 +18,15 @@ function Stacks(){
             <h2>Minhas <strong>Stacks</strong> e <strong>Tecnologias</strong></h2>
             <div className='list'>
                 <Image src={javascript} alt='logo javascript' className='item'/>
-                <Image src={typescript} alt='logo javascript' className='item'/>
-                <Image src={react} alt='logo javascript' className='item'/>
-                <Image src={html} alt='logo javascript' className='item'/>
-                <Image src={css} alt='logo javascript' className='item'/>
-                <Image src={mysql} alt='logo javascript' className='item'/>
-                <Image src={node} alt='logo javascript' className='item'/>
-                <Image src={next} alt='logo javascript' className='item'/>
-                <Image src={sass} alt='logo javascript' className='item'/>
-                <Image src={git} alt='logo javascript' className='item'/>
+                <Image src={typescript} alt='logo typescript' className='item'/>
+                <Image src={react} alt='logo react' className='item'/>
+                <Image src={html} alt='logo html' className='item'/>
+                <Image src={css} alt='logo css' className='item'/>
+                <Image src={tailwind} alt='logo tailwind' className='item'/>
+                <Image src={node} alt='logo nodejs' className='item'/>
+                <Image src={next} alt='logo nextjs' className='item'/>
+                <Image src={sass} alt='logo sass' className='item'/>
+                <Image src={git} alt='logo git' className='item'/>
             </div>
         </div>
     )
